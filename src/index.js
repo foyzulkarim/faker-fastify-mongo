d7b8115dcea8a3a69896e864518a1c9228d46f0f
Foyzul Karim <foyzulkarim@gmail.com>
2022-01-01 22:45:40 +1100

    added customers endpoint

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ fastify.register(require('fastify-mongodb'), {
     url: 'mongodb://192.168.122.86:27017/appdb'
 })
 
-fastify.register(require('fastify-axios'))
+// products endpoint
 
-const getFake = () => {
+const getFakeUniqueProductName = () => {
     return faker.fake("{{commerce.productName}} {{datatype.uuid}}");
 }
 
-const getProduct = () => {
+const getFakeProduct = () => {
     return {
-        name: faker.unique(getFake),
+        name: faker.unique(getFakeUniqueProductName),
         price: faker.commerce.price(),
         product: faker.commerce.product(),
         description: faker.commerce.productDescription(),
@@ -28,7 +28,7 @@ const getProduct = () => {
 const productsCollection = 'products'
 
 fastify.post(`/${productsCollection}`, function (req, reply) {
-    const p = getProduct();
+    const p = getFakeProduct();
     const collection = this.mongo.db.collection(productsCollection)
     collection.insertOne(p, (err, result) => {
         if (err) {
@@ -64,6 +64,74 @@ fastify.get(`/${productsCollection}/:id`, function (req, reply) {
         reply.send(result)
     })
 });
+
+
+
+// customers endpoint
+
+const getFakeCustomerName = () => {
+    return faker.fake("{{name.firstName}} {{name.lastName}}");
+}
+
+const getFakeUniqueEmail = () => {
+    return faker.fake("{{datatype.uuid}}.{{internet.email}}");
+}
+
+const getFakeCustomer = () => {
+    return {
+        name: getFakeCustomerName(),
+        email: faker.unique(getFakeUniqueEmail),
+        address: faker.address.streetAddress(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        zip: faker.address.zipCode(),
+        phone: faker.phone.phoneNumber(),
+        avatar: faker.image.avatar(),
+        country: faker.address.country(),
+        createdAt: faker.date.past(),
+    }
+}
+
+const customersCollection = 'customers'
+
+fastify.post(`/${customersCollection}`, function (req, reply) {
+    const c = getFakeCustomer();
+    const collection = this.mongo.db.collection(customersCollection)
+    collection.insertOne(c, (err, result) => {
+        if (err) {
+            console.log(err);
+            reply.send(err)
+        } else {
+            reply.send(result)
+        }
+    });
+})
+
+fastify.get(`/${customersCollection}`, function (req, reply) {
+    const collection = this.mongo.db.collection(customersCollection)
+    collection.find({}, {}).limit(100).toArray((err, result) => {
+        if (err) {
+            reply.send(err)
+            return
+        }
+
+        reply.send(result)
+    })
+});
+
+fastify.get(`/${customersCollection}/:id`, function (req, reply) {
+    const collection = this.mongo.db.collection(customersCollection)
+    const id = this.mongo.ObjectId(req.params.id)
+    console.log(id);
+    collection.find({ _id: id }, {}).toArray((err, result) => {
+        if (err) {
+            reply.send(err)
+            return
+        }
+        reply.send(result)
+    })
+});
+
 
 fastify.get('/', function (req, reply) {
     reply.send({ "success": true, time: new Date() });
