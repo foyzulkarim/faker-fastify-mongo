fdd284ec47a4ab538c2aca049e22c325946221c8
Foyzul Karim <foyzulkarim@gmail.com>
2022-01-01 23:58:45 +1100

    added loop in autocannon script

--- src/autocannon-helper.js
+++ src/autocannon-helper.js
@@ -1,8 +1,8 @@
 const autocannon = require('autocannon')
 
-const url =  'http://192.168.122.162'
+const url = 'http://192.168.122.162'
 
-const options = {
+const productEntryOptions = {
     url,
     connections: 10, //default
     pipelining: 1, // default
@@ -11,10 +11,16 @@ const options = {
         {
             method: 'POST',
             path: '/products',
-            // body: JSON.stringify({ name: Date.now().toString(36) }),
-            onResponse: (status, body, context) => {
-                console.log(status, body);
-            }
+            // onResponse: (status, body, context) => {
+            //     console.log(status, 'products', body);
+            // }
+        },
+        {
+            method: 'POST',
+            path: '/customers',
+            // onResponse: (status, body, context) => {
+            //     console.log(status, 'customers', body);
+            // }
         }
     ]
 }
@@ -23,4 +29,13 @@ const finishedBench = (err, result) => {
     console.log(result)
 }
 
-autocannon(options, finishedBench);
+// autocannon(options, finishedBench);
+let count = 0;
+setInterval(() => {
+    console.log('starting benchmark', count);
+    autocannon(productEntryOptions, finishedBench);
+    console.log('finished benchmark', count++);
+    if (count === 20) {
+        process.exit(0)
+    }
+}, 12000);
